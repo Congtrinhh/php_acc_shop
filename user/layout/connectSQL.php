0428dfcc049191a2ec9185aac5c17ebc42ba4785
Congtrinhh <trinhquycong@gmail.com>
2022-05-16 09:33:34 +0700

    xong authentication.php

---
+++ user/layout/connectSQL.php
@@ -0,0 +1,12 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "acc_smartphone";
+    
+    $conn = mysqli_connect($host, $username, $password, $database);
+    
+    if ($conn-> connect_error()) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
